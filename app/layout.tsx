cf81562b8592b6a12a62bdaf1d5a385c6dddb422
Nicolas Soroka <sorokanicolas@gmail.com>
2023-10-14 00:26:11 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlor",
+  description:
+    "description: plataforma copia de stackoverflow hecha con next js 13.5 con fines educativos",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
